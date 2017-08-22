6d16d54b07244ca024d04531657570ec516603cd
KkYuen <opooho@qq.com>
2017-08-22 16:54:33 +0800

    audio Code

---
+++ ElectricWave/ElectricWave/Category/KKNSString/NSString+Time2Code.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Time2Code.h
+//  ElectricWave
+//
+//  Created by SuperDry on 21/08/2017.
+//  Copyright © 2017 Michio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Time2Code)
+
+@end
---
+++ ElectricWave/ElectricWave/Category/KKNSString/NSString+Time2Code.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Time2Code.m
+//  ElectricWave
+//
+//  Created by SuperDry on 21/08/2017.
+//  Copyright © 2017 Michio. All rights reserved.
+//
+
+#import "NSString+Time2Code.h"
+
+@implementation NSString (Time2Code)
+
+@end
